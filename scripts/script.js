50011e115625e3463c6f765360a63f0b5c358fd7
Gabriel <gabrielpachecotec@gmail.com>
2024-09-15 13:37:27 +0100

    Finish playRound() functionality and convert user input choice into lowercase

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,53 @@ function getComputerChoice() {
 // Write the logic to get the human choice
 function getHumanChoice() {
     const humanChoice = prompt("Choose between rock, paper or scissors: ");
-    return humanChoice;
+    return humanChoice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        humanScore++;
+        computerScore++;
+        console.log(`Tie! You both chose ${humanChoice}`);
+        return;
+    }
+
+    switch (humanChoice) {
+        case 'rock':
+            if (computerChoice == 'paper') {
+                computerScore++;
+                console.log("You lose! Paper beats Rock");
+            } else {
+                humanScore++;
+                console.log("You win! Rock beats Scissors");
+            }
+            break;
+        case 'paper':
+            if (computerChoice == 'scissors') {
+                computerScore++;
+                console.log("You lose! Scissors beats Paper");
+            } else {
+                humanScore++;
+                console.log("You win! Paper beats Rock");
+            }
+            break;
+        case 'scissors':
+            if (computerChoice == 'rock') {
+                computerScore++;
+                console.log("You lose! Rock beats Scissors");
+            } else {
+                humanScore++;
+                console.log("You win! scissors beats paper");
+            }
+    }
 }
 
 // Declare the players score variables
 let humanScore = 0;
 let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
