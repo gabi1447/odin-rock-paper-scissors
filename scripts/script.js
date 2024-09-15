f4a24f94561cf4ae10a417437710955ad05f3b42
Gabriel <gabrielpachecotec@gmail.com>
2024-09-15 13:01:05 +0100

    Finish functionality of getHumanChoice()

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 // This function should return 'rock', 'paper', 'scissors'.
 function getComputerChoice() {
     // Populating Choices array with all possible combinations.
-    choices = ['rock', 'paper', 'scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     // Generating random index
-    randomIndex = Math.floor(Math.random() * 100) % 3;
+    const randomIndex = Math.floor(Math.random() * 100) % 3;
     return choices[randomIndex];
 }
 
-console.log(getComputerChoice());
+// Write the logic to get the human choice
+function getHumanChoice() {
+    const humanChoice = prompt("Choose between rock, paper or scissors: ");
+    return humanChoice;
+}
